594692ae854d99579a5c4daea0cfbbed5716b114
jpmartinez10 <jpierre.s.martinez@gmail.com>
2024-12-04 14:05:40 -0500

    bit shifted condition flags

--- vm.c
+++ vm.c
@@ -5,6 +5,10 @@ Project : Virtual Machine LC3 Architecture
 Resource : https://www.jmeiners.com/lc3-vm/
 */
 
+
+#include <stdint.h>
+
+
 // 65536 Memory Locations
 #define MEMORY_MAX (1 << 16) // 1 bit shifted 16 times , 1 x 2^16
 uint16_t memory[MEMORY_MAX];    
@@ -30,5 +34,23 @@ enum
 uint16_t reg[R_COUNT];
 
 // Instruction Set
-
+enum
+{
+    OP_BR = 0, /* branch */
+    OP_ADD,    /* add  */
+    OP_LD,     /* load */
+    OP_ST,     /* store */
+    OP_JSR,    /* jump register */
+    OP_AND,    /* bitwise and */
+    OP_LDR,    /* load register */
+    OP_STR,    /* store register */
+    OP_RTI,    /* unused */
+    OP_NOT,    /* bitwise not */
+    OP_LDI,    /* load indirect */
+    OP_STI,    /* store indirect */
+    OP_JMP,    /* jump */
+    OP_RES,    /* reserved (unused) */
+    OP_LEA,    /* load effective address */
+    OP_TRAP    /* execute trap */
+};
 
