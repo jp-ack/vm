6bba339d3ed4321606860aaf6b8f5647d29ce39d
jpmartinez10 <jpierre.s.martinez@gmail.com>
2024-12-04 14:44:51 -0500

    e

--- vm.c
+++ vm.c
@@ -5,18 +5,22 @@ Project : Virtual Machine LC3 Architecture
 Resource : https://www.jmeiners.com/lc3-vm/
 */
 
-
+// Imports
+#include <stdio.h>
 #include <stdint.h>
+#include <signal.h>
+#include <Windows.h>
+#include <conio.h> 
 
 
 // 65536 Memory Locations
-#define MEMORY_MAX (1 << 16) // 1 bit shifted 16 times , 1 x 2^16
+#define MEMORY_MAX (1 << 16) 
+
 uint16_t memory[MEMORY_MAX];    
 
 // Registers
 enum
 {
-
     R_R0 = 0,
     R_R1,
     R_R2,
@@ -32,6 +36,14 @@ enum
 
 // Register Storage
 uint16_t reg[R_COUNT];
+
+// Condition Flags , Indicates Sign -/+ of previous calculation
+enum
+{   /* 0001 left shifted twice by "<<", 0001 -> 0100 : 1 -> 4 */
+    FL_POS = 1 << 0,  //POSITIVE 
+    FL_ZRO = 1 << 1,  //ZERO
+    FL_NEG = 1 << 2,  //NEGATIVE
+};
 
 // Instruction Set
 enum
@@ -53,4 +65,123 @@ enum
     OP_LEA,    /* load effective address */
     OP_TRAP    /* execute trap */
 };
+
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
+// Main Loop
+int main(int argc, const char* argv[])
+{
+    //Load Arguments
+    if (argc < 2)
+    {
+    printf("lc3 [image-file1] ...\n");
+    exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j) {
+        if (!read_image(argv[j]))
+        {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+
+    //Setup
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
+
+    /* since exactly one condition flag should be set at any given time, set the Z flag */
+    reg[R_COND] = FL_ZRO;
+
+    /* set the PC to starting position */
+    /* 0x3000 is the default */
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running)
+    {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+                {
+                    /* destination register (DR) */
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    /* first operand (SR1) */
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    /* whether we are in immediate mode */
+                    uint16_t imm_flag = (instr >> 5) & 0x1;
+
+                    if (imm_flag)
+                    {
+                        uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+                        reg[r0] = reg[r1] + imm5;
+                    }
+                    else
+                    {
+                        uint16_t r2 = instr & 0x7;
+                        reg[r0] = reg[r1] + reg[r2];
+                    }
+
+                    update_flags(r0);
+                }
+                break;
+            case OP_AND:
+                @{AND}
+                break;
+            case OP_NOT:
+                @{NOT}
+                break;
+            case OP_BR:
+                @{BR}
+                break;
+            case OP_JMP:
+                @{JMP}
+                break;
+            case OP_JSR:
+                @{JSR}
+                break;
+            case OP_LD:
+                @{LD}
+                break;
+            case OP_LDI:
+                @{LDI}
+                break;
+            case OP_LDR:
+                @{LDR}
+                break;
+            case OP_LEA:
+                @{LEA}
+                break;
+            case OP_ST:
+                @{ST}
+                break;
+            case OP_STI:
+                @{STI}
+                break;
+            case OP_STR:
+                @{STR}
+                break;
+            case OP_TRAP:
+                @{TRAP}
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                @{BAD OPCODE}
+                break;
+        }
+    }
+    @{Shutdown}
+}
+
 
