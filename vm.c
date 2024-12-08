a627fda90ea54c57901e4bff1e7d55f092bdb6f6
jpmartinez10 <jpierre.s.martinez@gmail.com>
2024-12-07 23:20:20 -0500

    finalized

--- vm.c
+++ vm.c
@@ -82,6 +82,30 @@ enum
     OP_TRAP    /* execute trap */
 };
 
+HANDLE hStdin = INVALID_HANDLE_VALUE;
+DWORD fdwMode, fdwOldMode;
+
+void disable_input_buffering()
+{
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hStdin, &fdwOldMode); /* save old mode */
+    fdwMode = fdwOldMode
+            ^ ENABLE_ECHO_INPUT  /* no input echo */
+            ^ ENABLE_LINE_INPUT; /* return when one or
+                                    more characters are available */
+    SetConsoleMode(hStdin, fdwMode); /* set new mode */
+    FlushConsoleInputBuffer(hStdin); /* clear buffer */
+}
+
+void restore_input_buffering()
+{
+    SetConsoleMode(hStdin, fdwOldMode);
+}
+
+uint16_t check_key()
+{
+    return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit();
+}
 
 
 void handle_interrupt(int signal)
@@ -423,7 +447,7 @@ int main(int argc, const char* argv[])
                 break;
         }
     }
-    restore_import_buffering();
+    restore_input_buffering();
 }
 
 
